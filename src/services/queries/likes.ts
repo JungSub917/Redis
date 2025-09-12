70688395e88744e2b9a4eba82c72feb22f416e94
JungSub917 <apple9568@naver.com>
2025-09-12 22:10:36 +0900

    좋아요 기능 추가: 사용자 좋아요 확인, 좋아요 추가/제거 및 좋아요한 아이템 목록 조회 로직 구현

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,9 +1,40 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { client } from '$services/redis';
+import { userLikesKey, itemsKey } from '$services/keys';
+import { getItems } from './items';
+import { get } from 'svelte/store';
 
-export const likedItems = async (userId: string) => {};
+export const userLikesItem = async (itemId: string, userId: string) => {
+  return client.sIsMember(userLikesKey(userId), itemId);
+};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likedItems = async (userId: string) => {
+  // user가 좋아요한 itemId 목록을 반환
+  const ids = await client.sMembers(userLikesKey(userId));
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+  // user가 가진 item Hash를 배열로 반황
+  return getItems(ids);
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+};
+
+export const likeItem = async (itemId: string, userId: string) => {
+  const inserted = await client.sAdd(userLikesKey(userId), itemId);
+
+  if (inserted) {
+    return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+  }
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+  const removed = await client.sRem(userLikesKey(userId), itemId);
+
+  if(removed){
+    return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+  }
+};
+
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+  const ids = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
+
+  return getItems(ids);
+
+};
